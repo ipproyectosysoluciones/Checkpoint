5bfb533393075f594a97764e36fc60895701722c
Bladimir Parra - IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-04-27 02:32:16 -0500

    commit CP-M1_4

---
+++ CPM1_4/checkpoint/04.js
@@ -0,0 +1,39 @@
+/* 4️⃣ ***** EJERCICIO 4 - RECURSIÓN ***** - seleccionarPelicula() 4️⃣
+
+¡Felicitaciones! Te han asignado el rol de manejar nuestro cine recién
+inagurado. Junto con este rol también te asignaron un objeto con los nombres de
+varias películas que tenemos dentro de Henry Buster. El público te dirá qué
+película quieren ver. Tu tarea es buscarla de forma recursiva y devolverla.
+
+
+⛔️ IMPORTANTE ⛔️
+1) Debes utilizar recursión.
+2) Si el objeto está vacío retornar el string: 'por favor indicar las peliculas
+   a buscar'.
+3) Si no se encuentra la pelicula retornar el string: 'no tenemos la pelicula:
+   "nombrePelicula"'.
+4) Si encuentras la película retornar el string: 'se encontro la pelicula:
+   "nombrePelicula"'.
+
+
+📝 EJEMPLO 📝
+
+LISTA
+{
+ 0: "El padrino", 1: "Iron Man", 2: "Terminator", 3: "Rambo", 4: "Creed"
+}
+
+INPUT ---> (LISTA, "Terminator") OUTPUT --> 'se encontro la pelicula:
+Terminator'
+
+💡 TIP 💡 Puedes definir un default-parameter para ayudarte.
+*/
+
+function seleccionarPelicula(peliculas, nombrePelicula, i = 0) {
+    // Tu código aquí:
+    
+}
+
+
+/*⚠️ NO MODIFICAR NADA DEBAJO DE ESTA LINEA ⚠️*/
+module.exports = seleccionarPelicula;
---
+++ CPM1_4/checkpoint/05.js
@@ -0,0 +1,47 @@
+/*5️⃣ ***** EJERCICIO 5 - RECURSIÓN ***** - peliTaquillera() 5️⃣
+
+Ya que eres el responsable administrativo de Henry Buster tienes que buscar
+entre todas las películas aquellas que son taquilleras (que más ganancias
+redituan). Para este ejercicio recibiras un arreglo de objetos, donde cada
+objeto es una película que tendrá la siguiente estructura:
+{
+  nombre: "Harry Potter y las reliquias de la muerte 2",
+  añoDeEstreno: "2011",
+  genero: "Fantasia/Aventura",
+  taquillera: true
+}
+
+
+⛔️ IMPORTANTE ⛔️
+1) Tienes que recorrer el arreglo de forma recursiva y devolver aquellas
+   películas que tengan la propidad "taquillera" igual a true.
+2) Si el arreglo está vacío retornar un arreglo vacío.
+
+
+📝 EJEMPLO 📝 INPUT --->
+[{
+ nombre: "Los pitufos", añoDeEstreno: "2011", genero:
+ "Fantasia/Aventura/Acción", taquillera: false
+ },
+ {
+  nombre: "Avengers: La era de Ultrón", añoDeEstreno: "2015", genero:
+  "Fantasia/Aventura/Acción", taquillera: true
+ }]
+
+OUTPUT --->
+[{
+  nombre: "Avengers: La era de Ultrón", añoDeEstreno: "2015", genero:
+  "Fantasia/Aventura/Acción", taquillera: true
+}]
+
+
+💡 TIP 💡
+1) Podes definir un default-parameter para ayudarte.
+*/
+function peliTaquillera(peliculas) {
+    // Tu código aquí:
+    
+}
+
+/*⚠️ NO MODIFICAR NADA DEBAJO DE ESTA LINEA ⚠️*/
+module.exports = peliTaquillera;
